8ba1f67365e8e73db2cfd2d655b4dce774707506
mary-brooks <maryemmabrooks@gmail.com>
2024-02-24 14:13:25 +0000

    implemented and tested get route to retrieve all budgets

--- routes/budgets.routes.js
+++ routes/budgets.routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const Budget = require('../models/Budget.model');
 const mongoose = require('mongoose');
 
-// CRUD Create: New budget
+// CRUD Create: Post new budget
 router.post('/budgets', async (req, res, next) => {
   const {
     name,
@@ -24,6 +24,16 @@ router.post('/budgets', async (req, res, next) => {
     });
 
     res.status(201).json(newBudget);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// CRUD Read: Get all budgets
+router.get('/budgets', async (req, res, next) => {
+  try {
+    const budgets = await Budget.find({});
+    res.status(200).json(budgets);
   } catch (error) {
     next(error);
   }
