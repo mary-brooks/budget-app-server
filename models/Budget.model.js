2c983763951b0df6f79782793a109ef503c48bee
mary-brooks <maryemmabrooks@gmail.com>
2024-02-24 11:29:57 +0000

    created budget model

---
+++ models/Budget.model.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const budgetSchema = new Schema({
+  name: { type: String, required: true },
+  startDate: { type: Date, default: Date.now },
+  endDate: Date,
+  totalIncome: { type: Number, min: 0, default: 0 },
+  savingsGoal: { type: Number, min: 0, default: 0 },
+  budgetAllocation: {
+    type: [
+      {
+        category: String,
+        amount: Number,
+      },
+    ],
+    default: [],
+  },
+});
+
+const Budget = model('Budget', budgetSchema);
+
+module.exports = Budget;
