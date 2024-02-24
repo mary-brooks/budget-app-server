6ea0eb15cc27cac93e3d453850fc35b77d2224a0
mary-brooks <maryemmabrooks@gmail.com>
2024-02-24 17:03:54 +0000

    implemented and tested get route to retrieve all transactions

--- routes/transactions.routes.js
+++ routes/transactions.routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const Transaction = require('../models/Transaction.model');
 
-// CRUD Create: Post new transaction
-router.post('/transactions', async (req, res, next) => {
-  const { amount, vendor, category, date, budgetId } = req.body;
+// CRUD Create: Post new transaction for a given budget
+router.post('/budgets/:budgetId/transactions', async (req, res, next) => {
+  const { budgetId } = req.params;
+  const { amount, vendor, category, date } = req.body;
 
   try {
     const newTransaction = await Transaction.create({
@@ -15,6 +16,18 @@ router.post('/transactions', async (req, res, next) => {
     });
 
     res.status(201).json(newTransaction);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// CRUD Read: Get all transactions for a given budget
+router.get('/budgets/:budgetId/transactions', async (req, res, next) => {
+  const { budgetId } = req.params;
+
+  try {
+    const transactions = await Transaction.find({ budget: budgetId });
+    res.status(200).json(transactions);
   } catch (error) {
     next(error);
   }
