2851a2e5063413c7777fe9cb00cdcde0939e1ab4
mary-brooks <maryemmabrooks@gmail.com>
2024-02-26 21:41:34 +0000

    created login route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -38,11 +38,11 @@ router.post('/signup', async (req, res, next) => {
     // Check if user already exists in the database
     const userExists = await User.findOne({ email });
 
-    // If the user with the same email already exists, send an error response
+    // If a user with the same email already exists, send an error response
     if (userExists) {
       return res
         .status(400)
-        .json({ message: 'The provided email is already registered' });
+        .json({ message: 'The provided email is already registered.' });
     }
 
     // Encrypt the password
@@ -60,6 +60,50 @@ router.post('/signup', async (req, res, next) => {
     res
       .status(201)
       .json({ email: newUser.email, name: newUser.name, _id: newUser._id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST /auth/login - Verifies email and password and returns a JWT
+router.post('/auth/login', async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if email or password is provided as an empty string
+    if (email === '' || password === '') {
+      return res.status(400).json({ message: 'Provide email and password.' });
+    }
+
+    // Check if user already exists in the database
+    const user = await User.findOne({ email });
+
+    // If the user is not found, send an error response
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: 'Provided email is not registered.' });
+    }
+
+    // Compare the provided password with the one saved in the database
+    const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+
+    // If the password is correct, create a payload for the JWT with the user info
+    if (isPasswordCorrect) {
+      // DO NOT SEND THE HASHED PASSWORD
+      const payload = { _id: user._id, email: user.email, name: user.name };
+
+      // Create and send the JWT
+      const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+        algorithm: 'HS256', // algorithm we want to encrypt the token with
+        expiresIn: '6h', // time of validity of the JWT
+      });
+
+      // Send the token as the response
+      res.status(200).json({ authToken });
+    } else {
+      res.status(401).json({ message: 'Unable to authenticate the user.' });
+    }
   } catch (error) {
     next(error);
   }
